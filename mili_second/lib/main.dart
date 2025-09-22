52e6150817eeff14c1c80a45e39ca2d1e32b91b3
UmbaDumba <dkdi3104@gmail.com>
2025-09-22 20:52:20 +0900

    git branch 변경 확인

--- mili_second/lib/main.dart
+++ mili_second/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'git branch 변경 확인',
       theme: ThemeData(
         // This is the theme of your application.
         //
