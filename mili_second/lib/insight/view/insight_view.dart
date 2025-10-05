49042f7e63c4670c52145db488a0dc06e1ac6a78
ksm <1004reveca@gmail.com>
2025-10-05 22:53:45 +0900

    insight 작업

--- mili_second/lib/insight/view/insight_view.dart
+++ mili_second/lib/insight/view/insight_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '/insight/view/special_this_weeks.dart';
 import '/insight/view/engagement_analysis.dart';
 import '/insight/view/pattern_analysis_by_day_of_the_week.dart';
+import '/insight/view/weekly_changing_trends.dart';
 
 class InsightView extends StatefulWidget {
   const InsightView({super.key});
@@ -29,6 +30,7 @@ class _InsightViewState extends State<InsightView> {
   // 주간 변화 트렌드 데이터
   final String _weeklyChangingTrendsComment =
       '새로운 관심사가 기존 패턴을 자연스럽게 변화시키고 있어요!';
+  final String _totalUsageTime = '+2.3 시간';
 
   @override
   Widget build(BuildContext context) {
@@ -59,61 +61,9 @@ class _InsightViewState extends State<InsightView> {
           SizedBox(height: 20.h),
 
           // 주간 변화 트렌드
-          Container(
-            width: 362.w,
-            height: 304.h,
-            decoration: BoxDecoration(
-              color: Color(0xFFFFFFFF),
-              borderRadius: BorderRadius.circular(10.r),
-              border: Border.all(color: Color(0xFFA2A2A2), width: 1.w),
-            ),
-            child: Padding(
-              padding: EdgeInsets.only(top: 15.h, left: 20.w, right: 20.w),
-              child: Column(
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'assets/icons/weeklyChangeGraph.png',
-                        width: 42.w,
-                        height: 42.h,
-                      ),
-                      SizedBox(width: 10.w),
-                      Text(
-                        '주간 변화 트렌드',
-                        style: TextStyle(
-                          color: Color(0xFF000000),
-                          fontSize: 17.sp,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 5.h),
-                  Container(
-                    width: 310.w,
-                    height: 1.h,
-                    decoration: BoxDecoration(color: Color(0xFFBEBEBE)),
-                  ),
-                  SizedBox(height: 15.h),
-                  SizedBox(
-                    width: 280.w,
-                    child: Text(
-                      _weeklyChangingTrendsComment.replaceAllMapped(
-                        RegExp(r'([.!?])\s*'), // . 또는 ! 또는 ? 뒤의 공백까지 매칭
-                        (match) => '${match[1]}\n', // 그 기호 뒤에 줄바꿈 추가
-                      ),
-                      style: TextStyle(
-                        color: Color(0xFF000000),
-                        fontSize: 15.sp,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+          WeeklyChangingTrends(
+            comment: _weeklyChangingTrendsComment,
+            totalUsageTime: _totalUsageTime,
           ),
 
           SizedBox(height: 30.h),
