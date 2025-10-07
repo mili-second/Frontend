c0d6f3c9b6a2da5e666f9135c982a7383155fb31
UmbaDumba <dkdi3104@gmail.com>
2025-10-07 23:15:15 +0900

    token기반 자동로그인 기능 추가 (로그아웃은 아직 없음)

--- mili_second/lib/login_view.dart
+++ mili_second/lib/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mili_second/main_view.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -9,20 +10,49 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final storage = FlutterSecureStorage();
+
+  bool is_login = false;
+
+  Future<void> check_login() async {
+    print("로그인 확인");
+    String? value = await storage.read(key: "token");
+
+    setState(() {
+      is_login = (value != null);
+      print(value);
+    });
+  }
+
+  Future<void> write_token() async {
+    await storage.write(key: "token", value: "ok");
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    check_login();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => MainView()),
-            );
-          },
-          child: Text("test"),
-        ),
-      ),
-    );
+    return is_login
+        ? MainView()
+        : Scaffold(
+            body: Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  write_token();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainView()),
+                  );
+                },
+                child: Text("test"),
+              ),
+            ),
+          );
   }
 }
