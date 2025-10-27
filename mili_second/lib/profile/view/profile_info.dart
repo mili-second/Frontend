448f0b03e1bd40a45e00e8696615ac1457abca2b
ksm <1004reveca@gmail.com>
2025-10-27 19:21:16 +0900

    프로필 수정 삭제 작업

--- mili_second/lib/profile/view/profile_info.dart
+++ mili_second/lib/profile/view/profile_info.dart
@@ -229,29 +229,29 @@ class _EditProfileState extends State<ProfileInfo> {
               ],
             ),
           ),
-          Positioned(
-            top: kIsWeb ? 270 : 270.h,
-            left: kIsWeb ? 180 : 180.w,
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => EditProfile()),
-                );
-              },
-              child: Row(
-                children: [
-                  SizedBox(width: kIsWeb ? 70 : 70.w),
-                  Image.asset(
-                    'assets/icons/profile_edit.png',
-                    // widget.userProfileImage = 'assets/icons/profile_default.png' ? 'assets/icons/profileEdit.png' : ,
-                    width: kIsWeb ? 45 : 45.w,
-                    height: kIsWeb ? 45 : 45.h,
-                  ),
-                ],
-              ),
-            ),
-          ),
+          // Positioned(
+          //   top: kIsWeb ? 270 : 270.h,
+          //   left: kIsWeb ? 180 : 180.w,
+          //   child: GestureDetector(
+          //     onTap: () {
+          //       Navigator.push(
+          //         context,
+          //         MaterialPageRoute(builder: (context) => EditProfile()),
+          //       );
+          //     },
+          //     child: Row(
+          //       children: [
+          //         SizedBox(width: kIsWeb ? 70 : 70.w),
+          //         Image.asset(
+          //           'assets/icons/profile_edit.png',
+          //           // widget.userProfileImage = 'assets/icons/profile_default.png' ? 'assets/icons/profileEdit.png' : ,
+          //           width: kIsWeb ? 45 : 45.w,
+          //           height: kIsWeb ? 45 : 45.h,
+          //         ),
+          //       ],
+          //     ),
+          //   ),
+          // ),
         ],
       ),
     );
